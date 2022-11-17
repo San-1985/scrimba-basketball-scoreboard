6845ad90952a4c851a8b296332a1c42b42258a24
Sandra Hirsch <sandra.hirschgmail.com>
2022-11-17 10:51:33 +0100

    scrimba js project basketball scoreboard

---
+++ script.js
@@ -0,0 +1,35 @@
+let homeScore = 0;
+let guestScore = 0;
+
+function homeAdd1() {
+    homeScore += 1;
+    document.getElementById("home-score").innerText = homeScore;
+}
+
+function homeAdd2() {
+    homeScore += 2;
+    document.getElementById("home-score").innerText = homeScore;
+}
+
+function homeAdd3() {
+    homeScore += 3;
+    document.getElementById("home-score").innerText = homeScore;
+}
+
+function guestAdd1() {
+    guestScore += 1;
+    document.getElementById("guest-score").innerText = guestScore;
+
+}
+
+function guestAdd2() {
+    guestScore += 2;
+    document.getElementById("guest-score").innerText = guestScore;
+
+}
+
+function guestAdd3() {
+    guestScore += 3;
+    document.getElementById("guest-score").innerText = guestScore;
+
+}
